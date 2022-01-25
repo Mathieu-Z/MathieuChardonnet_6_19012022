5c10ba032feba0cb371abc1f7253c212e713510e
Mathieu Chardonnet <chardonnet.mathieu@gmail.com>
2022-01-25 19:25:27 +0100

    route + modele user

--- Docs fournis/API/back/app.js
+++ Docs fournis/API/back/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
 /*mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
@@ -8,7 +9,7 @@ const app = express();
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));*/
 
-const Thing = require('./models/thing');
+const Thing = require('./models/sauce');
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
@@ -31,50 +32,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-// publier une sauce
-app.post('/api/sauce', (req, res, next) => {
-  delete req.body._id;
-  const sauce = new Sauce({
-    ...req.body
-  });
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// trouve toutes les sauces
-app.get('/api/sauce', (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// trouve une sauce selon sont id
-app.get('/api/sauce/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
-
-// mise à jour d'une sauce
-app.put('/api/sauce/:id', (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// suppression d'une sauce
-app.delete('/api/sauce/:id', (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use('/api/sauces', stuffRoutes);
 
 module.exports = app;
---
+++ Docs fournis/API/back/routes/stuff.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', stuffCtrl.getAllSauces);
+router.post('/', stuffCtrl.createSauce);
+router.get('/:id', stuffCtrl.getOneSauce);
+router.put('/:id', stuffCtrl.modifySauce);
+router.delete('/:id', stuffCtrl.deleteSauce);
+
+module.exports = router;
