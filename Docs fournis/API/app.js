1250acda8af16686d7bbab0cd8c2d9a70d03ac1d
Mathieu Chardonnet <chardonnet.mathieu@gmail.com>
2022-02-10 10:56:54 +0100

    ajout limit de co

--- Docs fournis/API/app.js
+++ Docs fournis/API/app.js
@@ -12,25 +12,6 @@ mongoose.connect('mongodb+srv://MC:Projet6@projetoc.2amyr.mongodb.net/myFirstDat
 
 const Sauce = require('./models/sauce');
 
-/*app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});*/
-
 app.use(express.json());
 
 app.use((req, res, next) => {
