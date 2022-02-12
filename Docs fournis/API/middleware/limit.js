e568f8365e27487c14ce5d77f1c08c0c9c588270
Mathieu Chardonnet <chardonnet.mathieu@gmail.com>
2022-02-12 19:00:35 +0100

    maj presentation limit

--- Docs fournis/API/middleware/limit.js
+++ Docs fournis/API/middleware/limit.js
@@ -1,9 +1,9 @@
 const rateLimit = require("express-rate-limit")
 
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 3,
-    message: "Trop de tentatives de connexion. Compte bloqué pour 5 minutes"
+  windowMs: 5 * 60 * 1000,
+  max: 3,
+  message: "Trop de tentatives de connexion. Compte bloqué pour 5 minutes"
 })
 
 module.exports = { limiter }
